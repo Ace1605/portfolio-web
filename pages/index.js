b0afd9b93e74fc168dd70981fff45df69c84fe04
Ace1605 <46580446+Ace1605@users.noreply.github.com>
2023-12-19 01:59:16 +0100

    updated my works

--- pages/index.js
+++ pages/index.js
@@ -1,127 +1,130 @@
 import {
   Button,
-  Container, 
-  Box, 
-  Heading, 
+  Container,
+  Box,
+  Heading,
   Image,
   Link,
   useColorModeValue
 } from '@chakra-ui/react'
-import {ChevronRightIcon} from '@chakra-ui/icons'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 import { BioSection, BioYear } from '../components/bio'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
-import NextLink from 'next/link';
+import NextLink from 'next/link'
 import Layout from '../components/layouts/article'
-
-
-
 
 export default function Home() {
   return (
     <Layout>
-        <Container>
-          <Box 
-          borderRadius="lg" 
-          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} 
-          p={3} 
-          mb={6} 
-          mt={6} 
-          align="center" 
+      <Container>
+        <Box
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
+          mt={6}
+          align="center"
+        >
+          Hello, I&apos;m a front-end developer based in Nigeria
+        </Box>
+
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Achugo Ebuka
+            </Heading>
+            <p>Ace Devs (Frontend Developer)</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
           >
-            Hello,  I&apos;m a front-end developer based in Nigeria
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              objectFit="cover"
+              width="110px"
+              height="110px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/Ace.jpeg"
+              alt="author-image"
+            />
           </Box>
-
-          <Box display={{md:'flex'}} >
-              <Box flexGrow={1}>
-                <Heading as="h2" variant="page-title">
-                    Achugo Chika
-                </Heading>
-                <p>Ace Devs (Microbiologist / Developer)</p>
-              </Box>
-              <Box 
-                flexShrink={0} 
-                mt={{base: 4, md: 0}}
-                ml={{md: 6}} 
-                align="center" >
-                <Image 
-                borderColor="whiteAlpha.800" 
-                borderWidth={2} 
-                borderStyle="solid" 
-                objectFit="cover" 
-                width="110px" 
-                height="110px" 
-                display="inline-block" 
-                borderRadius="full" 
-                src="/images/Ace.jpeg" 
-                alt="author-image"/>
-              </Box>
-          </Box>
-          <Section delay={0.1}>
-            <Heading as="h3" variant="section-title" >
-              Work
-            </Heading>
-            <Paragraph>
-              Ace is a freelancer 
-              and with an employment contract
-              with The Hangout Technologies.
-              He has a passion for building digital services/softwares
-              that provide solution to real-life problems, loves
-              watching animes, gaming and swimming. Currently working
-              on a product called {' '}
-              <Link href="https://myhangout.co/" target="_blank">TheHangout</Link>
-              .
-            </Paragraph>
-            <Box align="center" my={4}>
-              <NextLink href="/works">
-                <Button rightIcon={<ChevronRightIcon/>} colorScheme = "blue">
+        </Box>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            Ace is an accomplished frontend developer currently working at
+            Chequebase and having previously worked at SmadeandSmight. With over
+            two years of experience, Ace excels in crafting digital solutions
+            that tackle real-world challenges. Specializing in modern frontend
+            technologies, including React, Redux, and REST, Ace has played a
+            pivotal role in projects such as TheHangout, actively contributing
+            to the development of high-quality and user-centric digital
+            services. Notably, Ace has leveraged Next.js, React, and Typescript
+            in the finance management platform{' '}
+            <Link href="https://chequebase.io" target="_blank">
+              Chequebase.io
+            </Link>
+            , demonstrating versatility and a mobile-first approach.{' '}
+            <Link href="https://myhangout.co/" target="_blank">
+              MyHangout
+            </Link>{' '}
+            is a lifestyle product developed during my tenure with
+            SmadeandSmight
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="blue">
                 My portfolio
-                </Button>
-              </NextLink>
-            </Box>
-          </Section>
-          <Section delay={0.2}>
-            <Heading as="h3" variant="section-title">
-              Bio
-            </Heading>
-            <BioSection>
-              <BioYear>1999</BioYear>
-              Born in Lagos, Nigeria.
-            </BioSection>
-            <BioSection>
-              <BioYear>2015</BioYear>
-              Completed the SSCE program in BrainPoint College
-              where i graduated with highest honors but couldn&apos;t
-              receive the award cause he chose to get a new phone over
-              going for the graduation ceremony
-            </BioSection>
-            <BioSection>
-              <BioYear>2018 to present</BioYear>
-                Undergraduate of Microbiology
-            </BioSection>
-            <BioSection>
-              <BioYear>2020 to present</BioYear>
-              Works as a freelancer
-            </BioSection>
-            <BioSection>
-              <BioYear>Present</BioYear>
-                Software Engineer for The Hangout Technologies
-            </BioSection>
-          </Section>
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1999</BioYear>
+            Born in Lagos, Nigeria.
+          </BioSection>
+          <BioSection>
+            <BioYear>2015</BioYear>
+            Completed the SSCE program in BrainPoint College where i graduated
+            with highest honors.
+          </BioSection>
+          <BioSection>
+            <BioYear>2018 to 2023</BioYear>
+            First Class Honours B.Sc Microbiology
+          </BioSection>
+          <BioSection>
+            <BioYear>2020 to 2022</BioYear>
+            Worked as a freelancer
+          </BioSection>
+          <BioSection>
+            <BioYear>2022 to present</BioYear>
+            Frontend Developer
+          </BioSection>
+        </Section>
 
-          <Section delay={0.2}>
-            <Heading as="h3" variant="section-title">
-              I ♡
-            </Heading>
-            <Paragraph>
-              Music, Game, {' '}
-                <Link href='https://animepahe.com' >Anime</Link>
-                , Car race, {''}
-                <Link href='https://animepahe.com' >Swimming</Link>
-                , building fun projects
-            </Paragraph>
-          </Section>
-        </Container>
-  </Layout>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            I ♡
+          </Heading>
+          <Paragraph>
+            Music, Game, <Link href="https://animepahe.com">Anime</Link>, Car
+            race, Swimming, building fun projects
+          </Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
